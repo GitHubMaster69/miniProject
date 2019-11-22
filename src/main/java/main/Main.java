44bc4f349b252d25cec90d2fb1afbf032aa0b5ff
Biggie Cheese <50361292+GitHubMaster69@users.noreply.github.com>
2019-11-22 14:09:36 +0100

    1.1

---
+++ src/main/java/main/Main.java
@@ -0,0 +1,16 @@
+package main;
+
+import processing.core.PApplet;
+
+public class Main extends PApplet {
+
+    public static void main(String[] args) {
+        PApplet.main("main.Main");
+    }
+
+public void draw() {
+
+    System.out.println("yeet");
+}
+  //  we out here
+}
